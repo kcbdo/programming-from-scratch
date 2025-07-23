eac2bd39934d1fb9930e27fb23c95faa97b91f59
kcbdo <167764220+kcbdo@users.noreply.github.com>
2025-07-23 13:49:23 +0000

    Confifuração do

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-
-function Home(){
-return <h1>Bom dia Paula! Se você me ama, então dê uma risadinha</h1>
+function Home() {
+  return <h1>Bom dia Paula! Se você me ama, então dê uma risadinha</h1>;
 }
-
 
 export default Home;
